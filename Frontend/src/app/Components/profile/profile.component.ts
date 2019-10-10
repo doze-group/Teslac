da4e05cea7a84b16020b1be80bf8d87230fcf683
Fernando Araujo <developgadget@gmail.com>
2019-10-10 12:38:17 -0500

    [Fernando Araujo][10-10-2019] chart

--- Frontend/src/app/Components/profile/profile.component.ts
+++ Frontend/src/app/Components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Chart from 'chart.js';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
+  ctx;
+
   constructor() { }
 
   ngOnInit() {
+    this.ctx = (document.getElementById('Chart') as HTMLCanvasElement).getContext('2d');
+    var myChart = new Chart(this.ctx, {
+      type: 'line',
+      data: {
+          labels: ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'],
+          datasets: [{
+              label: 'Actividad',
+              data: [12, 19, 3, 5, 2, 3, 0],
+          }]
+      },
+  });
   }
 
 }
