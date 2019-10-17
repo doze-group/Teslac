f9061eb3835793060a4e0467029054d9d96a28f4
Fernando Araujo <developgadget@gmail.com>
2019-10-17 12:08:57 -0500

    [Fernando Araujo][10-17-2019] reboot

---
+++ Frontend/src/app/+home-page/home-news/home-news.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ds-home-news',
+  styleUrls: ['./home-news.component.default.scss'],
+  templateUrl: './home-news.component.html',
+})
+
+export class HomeNewsComponent {
+
+}
